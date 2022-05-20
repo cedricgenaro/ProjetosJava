c65b23b860e54b0fcbefaa43837c5e1424195f3f
Olderico Cedric Genaro <101076293+cedricgenaro@users.noreply.github.com>
2022-05-20 18:37:10 -0300

    testes aula 13 java laço for

---
+++ TestesAula/RepeticaoFor/src/repeticaofor/RepeticaoFor.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package repeticaofor;
+
+/**
+ *
+ * @author Cedric
+ */
+public class RepeticaoFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*for (int cc = 1; cc <= 100; cc+=10) {
+        System.out.println(cc);
+        }*/
+        for (int i = 1; i <= 3; i++ ){
+            for (int j = 0; j <= 2; j+=2){
+                System.out.println(i +" "+j);
+            }
+        }
+    }
+
+}
