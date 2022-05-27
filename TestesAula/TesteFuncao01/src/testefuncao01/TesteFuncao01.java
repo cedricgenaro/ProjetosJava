1117decd7cea0cc0e74a9fe973419abffc3453e9
Olderico Cedric Genaro <101076293+cedricgenaro@users.noreply.github.com>
2022-05-26 21:02:18 -0300

    Fiz os teste da aula 15 de Java

---
+++ TestesAula/TesteFuncao01/src/testefuncao01/TesteFuncao01.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testefuncao01;
+
+/**
+ *
+ * @author Cedric
+ */
+public class TesteFuncao01 {
+
+    /**
+     * @param args the command line arguments
+     */
+     static int soma(int a, int b){
+        int s = a +b;
+        return s;
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int sm = soma(5,2);
+        System.out.println("A soma vale " + sm);
+    }
+    
+}
