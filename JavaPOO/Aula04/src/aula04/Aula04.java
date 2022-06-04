bb50c327833d4efeb9a80559611a04d6de389d00
Olderico Cedric Genaro <101076293+cedricgenaro@users.noreply.github.com>
2022-06-03 21:22:17 -0300

    Criei o ex 04 Java

---
+++ JavaPOO/Aula04/src/aula04/Aula04.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula04;
+
+/**
+ *
+ * @author Cedric
+ */
+public class Aula04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Caneta c1 = new Caneta("NIC",0.5f, "Amarelo");
+        
+        Caneta c2 = new Caneta("KKK", 1.5f, "Laranja");
+        
+        System.out.println(c1.getCor());
+        
+    }
+    
+}
