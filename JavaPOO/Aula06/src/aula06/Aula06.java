3c089d4f3f41e8cc8d5297ed392e7f6a68db7892
Olderico Cedric Genaro <101076293+cedricgenaro@users.noreply.github.com>
2022-06-12 14:25:57 -0300

    Criei aula 06 Java

---
+++ JavaPOO/Aula06/src/aula06/Aula06.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula06;
+
+/**
+ *
+ * @author Cedric
+ */
+public class Aula06 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ControleRemoto c = new ControleRemoto();
+        c.maisVolume();
+        c.play();
+        c.abrirMenu();
+        c.fecharMenu();
+    }
+    
+}
---
+++ JavaPOO/Aula06/src/aula06/ControleRemoto.java
@@ -0,0 +1,131 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula06;
+
+/**
+ *
+ * @author Cedric
+ */
+public class ControleRemoto implements Controlador {
+    //Atributos
+    private int volume;
+    private boolean ligado;
+    private boolean tocando;
+    
+    //Métodos Especiais
+    public ControleRemoto(){
+        this.volume = 50;
+        this.ligado = true;
+        this.tocando = false;
+    }
+
+    private int getVolume() {
+        return volume;
+    }
+
+    private boolean getLigado() {
+        return ligado;
+    }
+
+    private boolean getTocando() {
+        return tocando;
+    }
+
+   private void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    private void setLigado(boolean ligado) {
+        this.ligado = ligado;
+    }
+
+    private void setTocando(boolean tocando) {
+        this.tocando = tocando;
+    }
+    
+    //Métodos Abstratos
+
+    @Override
+    public void ligar() {
+        this.setLigado(true);
+    }
+    
+
+    @Override
+    public void desligar() {
+        this.setLigado(false);
+    }
+
+    @Override
+    public void abrirMenu() {
+        
+        System.out.println("----- MENU -----");
+        System.out.println("Está ligado? " + this.getLigado());
+        System.out.println("Está tocando? " + this.getTocando());
+        System.out.print("Volume: " + this.getVolume());
+        for(int i = 0; i <= this.getVolume(); i+=10){
+            System.out.print("|");
+        }
+        System.out.println("");
+    }
+
+    @Override
+    public void fecharMenu() {
+        System.out.println("Fechando Menu... ");
+    }
+
+    @Override
+    public void maisVolume() {
+        if(this.getLigado()){
+            this.setVolume(this.getVolume() + 5);
+        }else{
+            System.out.println("Impossivel aumentar volume");
+        }
+    }
+
+    @Override
+    public void menosVolume() {
+        if(this.getLigado()){
+            this.setVolume(this.getVolume() -5);
+        }else{
+            System.out.println("Impossível diminuir volume");
+        }
+      
+    }
+    
+    @Override
+    public void ligarMudo() {
+        if(this.getLigado() && this.getVolume() > 0){
+            this.setVolume(0);
+        }
+    }
+
+    @Override
+    public void desligarMudo() {
+        if(this.getLigado() && this.getVolume() == 0){
+            this.setVolume(50);
+        }
+    }
+
+    @Override
+    public void play() {
+        if(this.getLigado() && !(this.getTocando())){
+            this.setTocando(true);
+        }else{
+            System.out.println("Não consegui reproduzir");
+        }
+    }
+
+    @Override
+    public void pause() {
+        if(this.getLigado() && this.getTocando()){
+            this.setTocando(false);
+        }else{
+            System.out.println("Não consegui pausar");
+        }
+    }
+    
+    
+}
