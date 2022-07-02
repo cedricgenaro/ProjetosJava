500f41f2eacbf2328866bcff24e7778d60cac1c0
Olderico Cedric Genaro <101076293+cedricgenaro@users.noreply.github.com>
2022-07-02 15:41:52 -0300

    Fiz aula 11 Java POO

---
+++ JavaPOO/ProjetoPessoas/src/projetopessoas/Aluno.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projetopessoas;
+
+/**
+ *
+ * @author Cedric
+ */
+public class Aluno extends Pessoa{
+    private int matr;
+    private String curso;
+    
+    public void cancelarMatr(){
+        System.out.println("Matricula será cancelada");
+    }
+
+    public int getMatr() {
+        return matr;
+    }
+
+    public void setMatr(int matr) {
+        this.matr = matr;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+    
+    
+}
